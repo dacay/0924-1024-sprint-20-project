8ad147265b27e9f673025de33883699a1f9c6fe6
Deniz Acay <me@denizacay.com>
2025-07-19 12:19:49 +0300

    Initial commit

---
+++ api/src/main/java/tr/com/workintech/s20/app/AppApplication.java
@@ -0,0 +1,13 @@
+package tr.com.workintech.s20.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/tr/com/workintech/s20/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.workintech.s20.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
